babbed5236438a97d1a5aad88eac6df63d27a865
N-Mangano <140650659+N-Mangano@users.noreply.github.com>
2024-01-21 16:24:45 +0100

    My first commit

---
+++ src/main/java/de/aittr/g_27_data_owners/G27DataOwnersApplication.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_data_owners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G27DataOwnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G27DataOwnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/aittr/g_27_data_owners/G27DataOwnersApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_data_owners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G27DataOwnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
